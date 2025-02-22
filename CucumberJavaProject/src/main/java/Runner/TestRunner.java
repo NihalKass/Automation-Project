242ae88c4d7be5e11e497d3296de2c615e1d1319
Pappu <pappu.kr26@gmail.com>
2025-02-22 13:39:31 +0530

    deleted files

--- CucumberJavaProject/src/main/java/Runner/TestRunner.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-@RunWith(Cucumber.class)
-@CucumberOptions(
-  features = "Features"//folder name
-  ,glue={"stepDefinition"} //package name for step def
-  )
-public class TestRunner {}
